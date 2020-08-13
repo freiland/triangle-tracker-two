76af3b957e7d35f8de8c89a444c2bb7a5ca17e18
kolmansoifer <kolmansoifer@gmail.com>
2020-08-13 13:49:58 -0700

    Build form using card and card body

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    const oneInt = parseInt($("input#number1").val());
+    const twoInt = parseInt($("input#number2").val());
+    const threeInt = parseInt($("input#number3").val());
+  
+    if ()
+  
+  
+  
+  }
+}
